3cf61cc911c4ac0a2c425d7652a897206a8af025
filippov <filippov70@gmail.com>
2015-04-29 13:42:30 +0700

    убрал подложку

--- src/app.js
+++ src/app.js
@@ -9,51 +9,65 @@ var Converter = require('./cadXML2GeoJSON.js');
 StartParse();
 
 function StartParse() {
-	// https://bitbucket.org/surenrao/xml2json
+    // https://bitbucket.org/surenrao/xml2json
     // http://www.chrome-allow-file-access-from-file.com/
     var parsedData;
-	$.get('./testdata/doc8500717.xml', function(xml) {
-		//var json = $.xml2json(xml).CadastralBlocks;
-		// $("#data").html('<code>'+JSON.stringify(json)+'</code>');
-		//console.log(Converter.GeoJSON);
+    $.get('./testdata/doc8500717.xml', function (xml) {
+        //var json = $.xml2json(xml).CadastralBlocks;
+        // $("#data").html('<code>'+JSON.stringify(json)+'</code>');
+        //console.log(Converter.GeoJSON);
         parsedData = Converter.GeoJSON(xml);
-		//console.log();
+        //console.log();
 
-	}).success(
-		function() {
-				var vectorLayer = new ol.layer.Vector({
-					source : new ol.source.GeoJSON({
-						projection : 'EPSG:3857'// ,
-					    //object: parsedData
-					})
-				});
+    }).success(
+            function () {
 
-				var format = new ol.format.GeoJSON();
-				for (i = 0; i < parsedData.features.length; i++) {
+                var fill = new ol.style.Fill({
+                    color: 'rgba(255,255,255,0.4)'
+                });
+                var stroke = new ol.style.Stroke({
+                    color: '#3399CC',
+                    width: 1.25
+                });
+                var styles = [
+                    new ol.style.Style({
+                        fill: fill,
+                        stroke: stroke
+                    })
+                ];
+                var vectorLayer = new ol.layer.Vector({
+                    source: new ol.source.GeoJSON({
+                        projection: 'EPSG:3857'
+                        
+                    })
+                });
+
+                var format = new ol.format.GeoJSON();
+                for (i = 0; i < parsedData.features.length; i++) {
                     console.log(parsedData.features[i].geometry);
-					var geometryObj = format
-							.readGeometry(parsedData.features[i].geometry);
-					var feature = new ol.Feature({
-						geometry : geometryObj//,
-						//propA : parsedData.features[i].properties.cadnumber
-					});
-					vectorLayer.getSource().addFeature(feature);
-				}
+                    var geometryObj = format
+                            .readGeometry(parsedData.features[i].geometry);
+                    var feature = new ol.Feature({
+                        geometry: geometryObj//,
+                                //propA : parsedData.features[i].properties.cadnumber
+                    });
+                    vectorLayer.getSource().addFeature(feature);
+                }
 
-				var map = new ol.Map({
-					target : 'map',
-					layers : [
-                        new ol.layer.Tile({
-                            source: new ol.source.OSM()
-                        }),
-					vectorLayer ],
-					view : new ol.View({
-						center : [ 0, 0 ],
-						zoom : 7
-					})
-				});
-				map.getView().fitExtent(vectorLayer.getSource().getExtent(),
-						map.getSize());
-			});
+                var map = new ol.Map({
+                    target: 'map',
+                    layers: [
+//                        new ol.layer.Tile({
+//                            source: new ol.source.OSM()
+//                        }),
+                        vectorLayer],
+                    view: new ol.View({
+                        center: [0, 0],
+                        zoom: 7
+                    })
+                });
+                map.getView().fitExtent(vectorLayer.getSource().getExtent(),
+                        map.getSize());
+            });
 }
 
